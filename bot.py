d4f2409eae1125873828b021e35b5fb12c89689f
Smit Panchal <91816167+XSmitX@users.noreply.github.com>
2024-03-12 00:34:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,27 +2,57 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import asyncio
 import pymongo
-
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import InlineKeyboardButton as ikb, InlineKeyboardMarkup as ikm
 # Connect to MongoDB Atlas
 
 mongo_client = pymongo.MongoClient("mongodb+srv://smit:smit@cluster0.pjccvjk.mongodb.net/?retryWrites=true&w=majority")
 db = mongo_client["telegram_bot"]
 collection = db["users"]
+chat_ids_collection = db["chat_ids"]
+premium_collection = db["premium"]
 
 bot = Client(
-    "TeBot",
+    "TeBofsdfsf",
     bot_token="6348649632:AAEJ62K8vK11wbtMNixFh6M2s4-q7O2eBN0",
     api_id=17249531,
     api_hash="b67965c13be2164d8a2bb6d035a1076a"
 )
 
+channel_username = "@Sam_Ott_Store"
 
-broadcasting_tasks = {}  # Dictionary to store broadcasting tasks for each user
-DEFAULT_INTERVAL = 60  # Default interval in seconds
+def check_joined():
+    async def func(flt, bot, message):
+        join_msg = f"**To use this bot, Please join our channel.\nJoin From The Link Below 👇**"
+        user_idd = message.from_user.id
+        chat_idd = message.chat.id
+        try:
+            member_info = await bot.get_chat_member(channel_username, user_idd)
+            if member_info.status in (ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER):
+                return True
+            else:
+                await bot.send_message(chat_idd, join_msg , reply_markup=ikm([[ikb("✅ Join Channel", url=f"https://t.me/Sam_Ott_Store")]]))
+                return False
+        except Exception as e:
+            await bot.send_message(chat_idd, join_msg , reply_markup=ikm([[ikb("✅ Join Channel", url="https://t.me/Sam_Ott_Store")]]))
+            return False
 
-@bot.on_message(filters.command('start') & filters.private)
+    return filters.create(func)
+
+ADMIN_ID = [6121699672, 6971497666]
+# Dictionary to store broadcasting tasks for each user
+broadcasting_tasks = {}
+# Default interval in seconds
+DEFAULT_INTERVAL = 60
+
+@bot.on_message(filters.command('start') & check_joined())
 async def start(bot, message):
-    await bot.send_message(message.chat.id, "Welcome to Message Forwarder Bot!")
+    if check_joined():
+        user_id = message.from_user.id
+        if not collection.find_one({"user_id": user_id}):
+            collection.insert_one({"user_id": user_id})
+        await bot.send_message(message.chat.id, "Welcome to Message Forwarder Bot!")
+
 
 @bot.on_message(filters.command("msg") & filters.private)
 async def set_message(bot, message):
@@ -32,40 +62,59 @@ async def set_message(bot, message):
     await bot.send_message(message.chat.id, "Message set successfully.")
 
 @bot.on_message(filters.command("add") & filters.private)
-async def add_group_id(bot, message):
-    group_id = int(message.text.split()[1])
-    user_id = message.from_user.id
-    # Check if user already exists in the database
-    user = collection.find_one({"user_id": user_id})
-    if user:
-        # Update existing user's group IDs
-        collection.update_one({"user_id": user_id}, {"$addToSet": {"group_ids": group_id}})
-    else:
-        # Create new user entry in the database
-        collection.insert_one({"user_id": user_id, "group_ids": [group_id]})
-    await bot.send_message(message.chat.id, f"Group ID {group_id} added successfully.")
+async def add_chat_id(bot, message):
+    user_id = message.from_user.id 
+    if message.from_user.id not in ADMIN_ID:
+        await bot.send_message(message.chat.id, "Only the admin can add chat IDs.")
+        return
+    chat_id = int(message.text.split()[1])
+    chat_ids_collection.insert_one({"chat_id": chat_id})
+    await bot.send_message(message.chat.id, f"Chat ID {chat_id} added successfully.")
+ #   if user_id == ADMIN_ID:
+ #       chat_id = int(message.text.split()[1])
+ #       chat_ids_collection.insert_one({"chat_id": chat_id})
+ #       await bot.send_message(message.chat.id, f"Chat ID {chat_id} added successfully.")
+ #   else:
+ #       await bot.send_message(message.chat.id, "Only the admin can add chat IDs.")
 
-@bot.on_message(filters.command("broadcast") & filters.private)
+@bot.on_message(filters.command("adduser") & filters.private)
+async def add_user_to_premium(bot, message):
+    user_id = message.from_user.id
+    if message.from_user.id not in ADMIN_ID:
+        await bot.send_message(message.chat.id, "Only the admin can add chat IDs.")
+        return
+    try:
+        user_to_add_id = int(message.text.split()[1])
+        premium_collection.insert_one({"user_id": user_to_add_id})
+        await bot.send_message(message.chat.id, f"User {user_to_add_id} added to premium.")
+    except ValueError:
+        await bot.send_message(message.chat.id, "Invalid user ID provided.")
+ 
+
+@bot.on_message(filters.command("broadcast") & filters.private & check_joined())
 async def start_broadcast(bot, message):
     user_id = message.from_user.id
-    user = collection.find_one({"user_id": user_id})
-    if user:
-        group_ids = user.get("group_ids", [])
-        if not group_ids:
-            await bot.send_message(message.chat.id, "No group IDs added. Use /add to add group IDs.")
-            return
+    if is_user_premium(user_id):
+        user = collection.find_one({"user_id": user_id})
+        if user:
+            chat_ids = [chat["chat_id"] for chat in chat_ids_collection.find()]
+            if not chat_ids:
+                await bot.send_message(message.chat.id, "No chat IDs added.")
+                return
 
-        msg = user.get("msg", "")
-        if not msg:
-            await bot.send_message(message.chat.id, "Message not set. Use /msg command to set a message.")
-            return
+            msg = user.get("msg", "")
+            if not msg:
+                await bot.send_message(message.chat.id, "Message not set. Use /msg command to set a message.")
+                return
 
-        if user_id in broadcasting_tasks:
-            await bot.send_message(message.chat.id, "Broadcasting is already ongoing for this user.")
-            return
+            if user_id in broadcasting_tasks:
+                await bot.send_message(message.chat.id, "Broadcasting is already ongoing for this user.")
+                return
 
-        broadcasting_tasks[user_id] = asyncio.create_task(broadcast_message(bot, user_id, group_ids, msg))
-        await bot.send_message(message.chat.id, f"Broadcasting started with an interval of {DEFAULT_INTERVAL} seconds.")
+            broadcasting_tasks[user_id] = asyncio.create_task(broadcast_message(bot, user_id, chat_ids, msg))
+            await bot.send_message(message.chat.id, f"Broadcasting started with an interval of {DEFAULT_INTERVAL} seconds.")
+    else:
+        await bot.send_message(message.chat.id, f"You are not a premium user. \nContact @Sam_Hub_Op to upgrade to premium.\nYour User id is : <code>{user_id}</code>")
 
 @bot.on_message(filters.command("stop") & filters.private)
 async def stop_broadcast(bot, message):
@@ -77,11 +126,17 @@ async def stop_broadcast(bot, message):
     else:
         await bot.send_message(message.chat.id, "No broadcasting task found for this user.")
 
-async def broadcast_message(bot, user_id, group_ids, msg):
+def is_user_premium(user_id):
+    return premium_collection.find_one({"user_id": user_id}) is not None
+
+async def broadcast_message(bot, user_id, chat_ids, msg):
     try:
         while True:
-            for group_id in group_ids:
-                await bot.send_message(group_id, msg)
+            for chat_id in chat_ids:
+                try:
+                    await bot.send_message(chat_id, msg)
+                except:
+                    print(f"Failed to send message to chat {chat_id}. Skipping...")
             await asyncio.sleep(DEFAULT_INTERVAL)
     except asyncio.CancelledError:
         pass
